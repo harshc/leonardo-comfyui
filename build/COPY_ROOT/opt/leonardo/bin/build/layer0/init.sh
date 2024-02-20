43dd5f7be9a6ddebaf319d40d45137cb8c6b639f
Harsh Chiplonkar <harshc@live.com>
2024-02-20 01:01:53 -0500

    Remove xformers

--- build/COPY_ROOT/opt/leonardo/bin/build/layer0/init.sh
+++ build/COPY_ROOT/opt/leonardo/bin/build/layer0/init.sh
@@ -7,7 +7,4 @@ umask 002
 source /opt/leonardo/bin/build/layer0/common.sh
 source /opt/leonardo/bin/build/layer0/nvidia.sh
 
-
-$MAMBA_DEFAULT_RUN python /opt/leonardo/tests/assert-torch-version.py
-
 source /opt/leonardo/bin/build/layer0/clean.sh
