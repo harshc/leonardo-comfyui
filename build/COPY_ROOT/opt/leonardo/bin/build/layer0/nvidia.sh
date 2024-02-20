1766a4502d1bb654dd4cbe6f74df234ee66b8964
Harsh Chiplonkar <harshc@live.com>
2024-02-20 14:48:16 -0500

    Debug build

--- build/COPY_ROOT/opt/leonardo/bin/build/layer0/nvidia.sh
+++ build/COPY_ROOT/opt/leonardo/bin/build/layer0/nvidia.sh
@@ -5,6 +5,11 @@ build_nvidia_main() {
 }
 
 build_nvidia_install_comfyui() {
+    cuda_short_version=$(cut -d '.' -f 1,2 <<< "${CUDA_VERSION}")
+    printf "CUDA Version: " "${cuda_short_version}\n" 
+    printf "Mamba Default Env: " "${MAMBA_DEFAULT_ENV}\n"
+    printf "Pytorch Version: " "${PYTORCH_VERSION} \n"
+
     micromamba run -n comfyui ${PIP_INSTALL} \
         torch=="${PYTORCH_VERSION}" \
         nvidia-ml-py3
